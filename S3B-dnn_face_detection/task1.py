88d3933d0565fcb7d78bc75038636f5e6481a4db
sawzedong <zedongsaw@gmail.com>
2023-05-27 21:48:00 +0800

    Modify hint for task 1

--- S3B-dnn_face_detection/task1.py
+++ S3B-dnn_face_detection/task1.py
@@ -1,6 +1,6 @@
 """
 TASK 1
 Write a code that can detect and box up faces (using DNN) and eyes (using Haar Cascade).  
-Hint 1: DNN alone will not work as it cannot box up the eyes, only give a rough coordinate. 
+Hint 1: The name of the Haar Cascade is 'haarcascade_eye_tree_eyeglasses.xml'
 Hint 2: How can you increase the accuracy of the code to detect the eyes?
 """
